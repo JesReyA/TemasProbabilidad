59b55a9778c94400e252cae741260302acc0c325
JesReyA <jesreyav@gmail.com>
2026-05-16 01:16:34 -0600

    .

--- tema_cinco.py
+++ tema_cinco.py
@@ -7,17 +7,17 @@ from matplotlib import image as mpimg
 class TemaCinco:
     def __init__(self):
         self.cartas = []
-        self.definicion = "La probabilidad es un método por el cual se obtiene la frecuencia de un acontecimiento determinado mediante \nla realización de un experimento aleatorio, del que se conocen todos los resultados posibles,\n bajo condiciones suficientemente estables."
-        self.cartasTrebol = 12
-        self.cartasDiamante = 12
-        self.cartasCorazon = 12
-        self.cartasEspada = 12
-    
-
+        self.definicion = "La probabilidad de que ocurran dos o más eventos, se calcula multiplicando la probabilidad de cada evento."
+        self.cartasTrebol = 13
+        self.cartasDiamante = 13
+        self.cartasCorazon = 13
+        self.cartasEspada = 13
 
     def mazoCartas(self):
-        for i in range(1, 13):
+        for i in range(1, 14):
             for j in range(1, 5):
                 self.cartas.append(i, j)
+
+    
 
     
